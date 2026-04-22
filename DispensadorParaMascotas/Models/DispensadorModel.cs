80b11f0f7615b252f98e232ea1a201069262f365
jose javier <floresriverajosejavier@gmail.com>
2026-04-21 19:45:39 -0600

    feat: agrega validaciones y mejora lógica en DispensadorModel

--- DispensadorParaMascotas/Models/DispensadorModel.cs
+++ DispensadorParaMascotas/Models/DispensadorModel.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DispensadorParaMascotas.Model
 {
@@ -12,8 +10,14 @@ namespace DispensadorParaMascotas.Model
             get { return _capacidadMaxima; }
             set
             {
-                if (value > 0)
-                    _capacidadMaxima = value;
+                if (value <= 0)
+                    throw new ArgumentException("La capacidad máxima debe ser mayor que 0");
+
+                _capacidadMaxima = value;
+
+                // Ajusta el nivel actual si excede la nueva capacidad
+                if (_nivelActual > _capacidadMaxima)
+                    _nivelActual = _capacidadMaxima;
             }
         }
 
@@ -23,23 +27,38 @@ namespace DispensadorParaMascotas.Model
             get { return _nivelActual; }
             set
             {
-                if (value >= 0 && value <= CapacidadMaxima)
-                    _nivelActual = value;
+                if (value < 0)
+                    throw new ArgumentException("El nivel no puede ser negativo");
+
+                if (value > CapacidadMaxima)
+                    throw new ArgumentException("El nivel no puede superar la capacidad máxima");
+
+                _nivelActual = value;
             }
         }
-        
 
         public double ObtenerPorcentaje()
         {
+            if (CapacidadMaxima == 0)
+                return 0;
+
             return NivelActual / CapacidadMaxima;
         }
 
         public void RestarComida(double cantidad)
         {
-            if (NivelActual >= cantidad)
-                NivelActual -= cantidad;
-            else
-                NivelActual = 0;
+            if (cantidad < 0)
+                throw new ArgumentException("La cantidad no puede ser negativa");
+
+            NivelActual = Math.Max(0, NivelActual - cantidad);
+        }
+
+        public void AgregarComida(double cantidad)
+        {
+            if (cantidad < 0)
+                throw new ArgumentException("La cantidad no puede ser negativa");
+
+            NivelActual = Math.Min(CapacidadMaxima, NivelActual + cantidad);
         }
     }
 }
